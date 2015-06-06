e2f544a180e37111883298f6f0d1d9bb17069423
NP-HARD <np-hard@localhost.localdomain>
2015-06-06 03:41:44 -0500

    JJV - Fixing scope of error enums

--- error.h
+++ error.h
@@ -6,19 +6,19 @@
 #define __QUANTUM_ERROR_H
 
 namespace Quantum {
+enum {
+	QUANTUM_SUCCESS   = 0,
+	QUANTUM_FAILURE   = 1,
+	QUANTUM_ENOMEM    = 2,
+	QUANTUM_EMLARGE   = 3,
+	QUANTUM_EMSIZE    = 4,
+	QUANTUM_EHASHFULL = 5,
+	QUANTUM_EMCMATRIX = 65536,
+	/* internal errors start at 65536 */
+	QUANTUM_EOPCODE   = 65537
+};
 class Error {
 	public:
-		enum {
-			QUANTUM_SUCCESS   = 0,
-			QUANTUM_FAILURE   = 1,
-			QUANTUM_ENOMEM    = 2,
-			QUANTUM_EMLARGE   = 3,
-			QUANTUM_EMSIZE    = 4,
-			QUANTUM_EHASHFULL = 5,
-			QUANTUM_EMCMATRIX = 65536,
-			/* internal errors start at 65536 */
-			QUANTUM_EOPCODE   = 65537
-		};
 	
 		static void errorHandler(void *f(int));
 		static const char *strerr(int errno);
