5db949d97ce061f7bb5bf54db5807ba82248a3ef
NP-HARD <np-hard@localhost.localdomain>
2015-06-30 23:24:36 -0500

    JJV - removed extranious target property from entangledPair

--- entangledPair.h
+++ entangledPair.h
@@ -18,7 +18,6 @@ class EntangledPair {
 		 * a_00|00> + a_01|01> + a_10|10> + a_11|11>
 		 */
 		Matrix* entanglementAmplitudes;
-		int target;
 		bool _isNull;
 
 	public:
@@ -28,7 +27,6 @@ class EntangledPair {
 		COMPLEX_FLOAT get(int a, int b);
 		void setEntanglements(COMPLEX_FLOAT a_00, COMPLEX_FLOAT a_01, 
 			COMPLEX_FLOAT a_10, COMPLEX_FLOAT a_11);
-		int getTarget();
 		bool isNull();
 };
 }
