68da2affc6499de0b5079119ed48c7afd9d07df2
NP-HARD <np-hard@localhost.localdomain>
2015-07-30 22:39:07 -0500

    JJV - Comments on nodes, silentEvil, and systemMessages

--- silentEvil.h
+++ silentEvil.h
@@ -1,5 +1,10 @@
 /*
  * silentEvil.h
+ * SilentEvil is an eavesdropper who simply listens to incoming traffic,
+ * randomly measures qubits at a rate defined by evilness.  This represents
+ * the attacker with no presence or purpose other than to analyze efficiency
+ * of quantum algorithms in detecting eavesdropping.
+ * SilentEvil implements the iEvil interface.
  */
 #ifndef __QUANTUM_SILENTEVIL_H
 #define __QUANTUM_SILENTEVIL_H
@@ -9,10 +14,15 @@
 namespace Quantum {
 class SilentEvil : public iEvil {
 	private:
+		//the rate of measurement for quantum channels
 		float evilness = 0.1;
 	public:
-		virtual void doEvil(shared_ptr<iRegister> reg, SystemMessage message);
+		//doEvil - listen in on a message
+		virtual void doEvil(shared_ptr<iRegister> reg, 
+			SystemMessage message);
+		//set the rate of random measurement for quantum channels
 		void setEvilness(float newEvilness);
+		//get the rate of random measurement for quantum channels
 		float getEvilness();
 };
 }
