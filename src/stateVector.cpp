c505656a608543acf078e469eb17391844c6bc6a
vanbrandwijk <vanbrandwijk@gmail.com>
2016-04-09 21:16:49 -0500

    JJV - Comments for stateVector

--- src/stateVector.cpp
+++ src/stateVector.cpp
@@ -1,12 +1,14 @@
 /*
  * stateVector.cpp
  */
+//STD C++ includes
 #include <algorithm>
 #include <complex.h>
 #include <math.h>
 #include <stdlib.h>
 #include <vector>
 
+//QooSim Includes
 #include "channelService_client.h"
 #include "matrix.h"
 #include "qubit.h"
@@ -15,41 +17,85 @@
 #include "stateVector.h"
 #include "stateVectorOperation.h"
 
+//External Includes
 #include "externals/jacobi_eigenvalue.hpp"
 
 using namespace std;
 
 namespace Quantum {
+/*
+ * StateVector
+ * Parameterless constructor, creates a StateVector of width 1
+ */
 StateVector::StateVector() : StateVector(1) {
 }
 
+/*
+ * StateVector
+ * @param bitWidth the width of the vector
+ * Constructor, creates a vector with width equal to bitWidth
+ */
 StateVector::StateVector(int bitWidth) 
 	: qsv(1, pow(2, bitWidth)), remoteQubits(bitWidth) {
 	this->qsv.set(0, 0, 1);
 }
 
+/*
+ * StateVector
+ * @param m a matrix object
+ * Constructor, creates a vector from the matrix object
+ */
 StateVector::StateVector(Matrix m) 
 	: qsv(m), remoteQubits(log(m.getRows())/log(2)) {
 }
 
+/*
+ * setIndex
+ * @param newIndex the input index
+ * Sets the memory map index of this vector
+ */
 void StateVector::setIndex(int newIndex) {
 	this->index = newIndex;
 }
 
+/* 
+ * getIndex
+ * @return the memory map index of this vector
+ */
 int StateVector::getIndex() {
 	return this->index;
 }
 
+/*
+ * resize
+ * @param newSize
+ * Add space the the remoteQubits vector
+ */
+void StateVector::resize(int newSize) {
+	this->remoteQubits.resize(newSize);
+}
+
+/*
+ * applyOperation
+ * @param m operation a matrix operator
+ * @param input1 the position of the qubit to be operated on
+ * Shortcut method to apply an operation to one qubit already in
+ * this vector
+ */
 void StateVector::applyOperation(Matrix operation, int input1) {
 	vector<int> inputs(1);
 	inputs.at(0) = input1;
 	this->applyOperation(operation, inputs);
 }
 
-void StateVector::resize(int newSize) {
-	this->remoteQubits.resize(newSize);
-}
-
+/*
+ * applyOperation
+ * @param m operation a matrix operator
+ * @param input1 the position of a qubit to be operated on
+ * @param input2 the position of a qubit to be operated on
+ * Shortcut method to apply an operation to two qubits already in
+ * this vector
+ */
 void StateVector::applyOperation(Matrix operation, int input1, int input2) {
 	vector<int> inputs(2);
 	inputs.at(0) = input1;
@@ -57,10 +103,24 @@ void StateVector::applyOperation(Matrix operation, int input1, int input2) {
 	this->applyOperation(operation, inputs);
 }
 
+/*
+ * applyOperation
+ * @param m operation a matrix operator
+ * @param inputs the positions of qubits to be operated on
+ * Apply an operation to an arbitrary number of qubits in this vector
+ */
 void StateVector::applyOperation(Matrix operation, vector<int> inputs) {
 	this->applyOperation(operation, inputs, true);
 }
 
+/*
+ * applyOperation
+ * @param operation a matrix operator
+ * @param inputs the positions of qubits to be operated on
+ * @param addToHistory whether or not to push this operation onto the
+ * vector's history
+ * Apply an operation to an arbitrary number of qubits in this vector
+ */
 void StateVector::applyOperation(Matrix operation, vector<int> inputs,
 	bool addToHistory) {
 	Matrix expandedOperation = operation;
@@ -90,6 +150,13 @@ void StateVector::applyOperation(Matrix operation, vector<int> inputs,
 	}
 }
 
+/*
+ * applyOperation
+ * @param operation a matrix operator
+ * @param inputs the qubit objects on which to apply the operation
+ * Apply an operation to an arbitrary number of qubits, consolidating them
+ * into this vector first
+ */
 void StateVector::applyOperation(Matrix operation, 
 	vector< shared_ptr<Qubit> > inputs) {
 	unsigned int i, j;
@@ -125,6 +192,13 @@ void StateVector::applyOperation(Matrix operation,
 	this->resize(this->getWidth());
 }
 
+/*
+ * generateRowMap
+ * @param inputs the inputs to generate a map for
+ * @return integer vector of the rowmap
+ * Generate a mapping of the rows in this vector in order to place the 
+ * inputs provided in the left most positions
+ */
 vector<int> StateVector::generateRowMap(vector<int> inputs) {
 	vector<int> positionMap(this->getWidth(),-1);
 	vector<int> rowMap(this->qsv.getRows(),0);
@@ -154,15 +228,28 @@ vector<int> StateVector::generateRowMap(vector<int> inputs) {
 	return rowMap;
 }
 
+/*
+ * getWidth
+ * @returns the bit width of this vector
+ */
 int StateVector::getWidth() {
 	return log(this->qsv.getRows()) / log(2);
 }
 
+/*
+ * print
+ * Prints this vector
+ */
 void StateVector::print() {
 	printf("Index: %i\r\n", this->index);
 	this->qsv.print();
 }
 
+/*
+ * reduce
+ * Removed unentanged qubits from this vector, splitting them off
+ * into their own vectors
+ */
 void StateVector::reduce() {
 	int i, j, k, firstValueFound, currentValue;
 	double DOUBLE_ZERO = .000001;
@@ -233,11 +320,24 @@ void StateVector::reduce() {
 	}
 }
 
+/*
+ * isBitSet
+ * @param index The index in the state vector
+ * @param position The bit position
+ * @return bool whether or not the bit position is set to zero or one
+ */
 bool StateVector::isBitSet(int index, int position) {
 	return (int ( index / pow(2, (position)) ) 
 			% 2 == 1 );
 }
 
+/* 
+ * getAlpha
+ * @param position
+ * @returns alpha value for this bit
+ * Determine the overall alpha (0) probability amplitude of the 
+ * bit position in this vector
+ */
 double StateVector::getAlpha(int position) {
 	double alpha = 0.0;
 	int i;
@@ -251,8 +351,15 @@ double StateVector::getAlpha(int position) {
 	}
 
 	return alpha;
-}	
+}
 
+/* 
+ * getBeta
+ * @param position
+ * @returns beta value for this bit
+ * Determine the overall beta (1) probability amplitude of the 
+ * bit position in this vector
+ */
 double StateVector::getBeta(int position) {
 	double beta = 0.0;
 	int i;
@@ -267,6 +374,12 @@ double StateVector::getBeta(int position) {
 	return sqrt(beta);
 }	
 
+/*
+ * measure
+ * @param position the bit to measure
+ * @return the measured value
+ * Perform a pseudo random measurement of the bit given
+ */
 int StateVector::measure(int position) {
 	int value = 0;
 	double measurement = rand() / (float)RAND_MAX;
@@ -278,10 +391,26 @@ int StateVector::measure(int position) {
 	return this->measure(position, value);
 }
 
+/*
+ * measure
+ * @param position the bit to measure
+ * @param forceResult
+ * @return the measured value
+ * Perform a forced measurement of the bit given
+ */
 int StateVector::measure(int position, int forceResult) {
 	this->measure(position, forceResult, true);
 }
 
+/*
+ * measure
+ * @param position the bit to measure
+ * @param forceResult
+ * @param propagate
+ * @return the measured value
+ * Perform a forced measurement of the bit given, 
+ * propagating the measuredment to any remote peers
+ */
 int StateVector::measure(int position, int forceResult, 
 	bool propagate) {
 	int i;
@@ -325,6 +454,11 @@ int StateVector::measure(int position, int forceResult,
 	return forceResult;
 }
 
+/*
+ * fromDensity
+ * @param rho the density matrix
+ * Convert a density matrix to a state vector
+ */
 void StateVector::fromDensity(Matrix rho) {
 	int i, j;
 	int N = rho.getRows();
@@ -358,6 +492,11 @@ void StateVector::fromDensity(Matrix rho) {
 	}
 }
 
+/*
+ * toDensity
+ * @return the density matrix
+ * Convert this state vector to a density matrix
+ */
 Matrix StateVector::toDensity() {
 	int i, j;
 	Matrix rho(this->qsv.getRows(), this->qsv.getRows());
@@ -372,6 +511,10 @@ Matrix StateVector::toDensity() {
 	return rho;
 }
 
+/*
+ * normalize
+ * Normalize probability amplitudes across the state vector
+ */
 void StateVector::normalize() {
 	int i;
 	double realPart = 0.0;
@@ -398,10 +541,18 @@ void sync() {
 
 }
 
+/*
+ * toMatrix
+ * @return this vector as a matrix
+ */
 Matrix StateVector::toMatrix() {
 	return this->qsv;
 }
 
+/*
+ * replay
+ * Replay this state vector's history
+ */
 void StateVector::replay() {
 	int i = 0;
 
